21879d5b782af1e4fc9e30da90db1c7236e9f813
hejzbh <tulumovichazim18@gmail.com>
2026-02-04 23:14:41 +0100

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/react-portfolio-template/',
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
-                    }
-                }
-            }
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    },
-})
+  base: "/",
+  plugins: [react()],
+});
